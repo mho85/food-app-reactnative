d4672835cdd377e392f2930ec583078dc29a2882
Michel Ho <mho85@icloud.com>
2020-06-09 11:38:22 +0200

    First commit

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const DetailsScreen = () => {
+
+    return (
+        <View>
+            <Text>DetailsScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default DetailsScreen;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+
+    return (
+        <View>
+            <Text>SearchScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
