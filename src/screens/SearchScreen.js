bfce355439b47ba4c073447b6169d14a4d062cc5
Michel Ho <mho85@icloud.com>
2020-06-19 13:28:26 +0200

    Call Yelp API

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [searchInput, setSearchInput] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchInput,
+                    location: 'paris'
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    };
 
     return (
         <View style={styles.viewStyle}>
             <SearchBar
                 input={searchInput}
-                onInputChange={(t) => setSearchInput(t)}
-                onInputSubmit={() => console.log("Lancer la recherche à partir du mot " + searchInput)}
+                onInputChange={setSearchInput}
+                onInputSubmit={searchApi}
             />
-            {/* <Text>{searchInput}</Text> */}
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>Found: {results.length} result(s)</Text>
         </View>
     );
 };
